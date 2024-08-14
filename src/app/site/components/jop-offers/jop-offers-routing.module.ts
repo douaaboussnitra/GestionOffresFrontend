2badc98b5ffa85de8e31ca794a43e06fd25aec8a
douaaboussnitra <douaaboussnitra8@gmail.com>
2024-08-14 20:19:01 +0100

    details

--- src/app/site/components/jop-offers/jop-offers-routing.module.ts
+++ src/app/site/components/jop-offers/jop-offers-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { offersComponent } from './offers/offers.component';
 import { DetailsComponent } from './details/details.component';
+import { FormComponent } from './form/form.component';
 
 const routes: Routes = [
     {
@@ -9,6 +10,7 @@ const routes: Routes = [
       children:[
         { path: 'offers', component : offersComponent},
         { path: 'details', component : DetailsComponent},
+        { path: 'form', component : FormComponent},
       ]
       }
   ];
--- src/app/site/components/jop-offers/jop-offers.module.ts
+++ src/app/site/components/jop-offers/jop-offers.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { JopOffersRoutingModule } from './jop-offers-routing.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { DetailsComponent } from './details/details.component';
+import { FormComponent } from './form/form.component';
 
 
 @NgModule({
   declarations: [
     offersComponent,
-    DetailsComponent
+    DetailsComponent,
+    FormComponent
   ],
   imports: [
     CommonModule,
